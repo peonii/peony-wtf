58b9848093f676be3df39e151690bb2fc53bf141
peony <peony@greenhouse.local>
2023-05-11 22:52:08 +0200

    init

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+		fontFamily: {
+			sans: ['InterVariable', 'sans-serif'],
+			display: ['Krona One', 'serif']
+		}
+	},
+	plugins: [],
+}
