effdb9fb780e7670cd57c074f18ee2257bae4f6e
peony <peony@greenhouse.local>
2023-11-03 17:59:34 +0100

    stuff'

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,6 @@
 <script>
-	import { page } from "$app/stores";
-    import "../app.css";
-    import LocomotiveScrollProvider from 'svelte-locomotive-scroll'
+	import { page } from '$app/stores';
+	import '../app.css';
 </script>
-  
-<LocomotiveScrollProvider
-    options={{
-        smooth: true
-    }}
-    watch={[
-        $page
-    ]}
-    location={$page.url}
-    onLocationChange={(scroll) => scroll.scrollTo(0, { duration: 2, disableLerp: false })}
-    imageTarget={''}
->
-    <slot />
-</LocomotiveScrollProvider>
+
+<slot />
